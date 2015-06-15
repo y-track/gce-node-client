1591084d31390f2eb88050f70b58d6ef5808304a
Loïc MARIE <loic.marie54@gmail.com>
2015-06-15 12:24:42 +0200

    Suppression module management, modifs SDK

--- sdk.js
+++ sdk.js
@@ -3,7 +3,46 @@ var core = require('./core'),
     instances = require('./instances'),
     machineTypes = require('./machineTypes'),
     diskTypes = require('./diskTypes'),
-    zones = require('./zones');
+    zones = require('./zones'),
+    fs = require('fs'),
+    request = require('request-promise'),
+    sep = require('path').sep;
+
+var refreshToken = function(options){
+        
+        return request({
+            url: 'https://www.googleapis.com/oauth2/v3/token',
+            method: 'POST',
+            form: {
+                client_id: options.client_id,
+                client_secret: options.client_secret,
+                refresh_token: options.refresh_token,
+                grant_type: 'refresh_token'
+            },
+            json: true
+        }).then(function(data){
+            return Promise.resolve(data.access_token);
+        });
+}
+
+var getLocalCredentials = function(){
+  return new Promise(function (fulfill, reject){
+    fs.readFile(process.env.HOME + sep + '.config' + sep + 'gcloud' + sep + 'credentials', 'utf8', function (err, res){
+      if (err) reject(err);
+      else fulfill(JSON.parse(res));
+    });
+  });
+}
+
+var getCredentialsFromMetadata = function(){
+    return request({
+        url: 'http://metadata/computeMetadata/v1/instance/service-accounts/default/token',
+        method: 'GET',
+        headers: {
+            "Metadata-Flavor": "Google"
+        }
+    });
+}
 
 module.exports = {
 	instances: core.generateFunctions(instances.routes),
@@ -12,5 +51,8 @@ module.exports = {
     machineTypes: core.generateFunctions(machineTypes.routes),
     diskTypes: core.generateFunctions(diskTypes.routes),
     getToken: core.getToken,
+    refreshToken: refreshToken,
+    getCredentialsFromMetadata: getCredentialsFromMetadata,
+    getLocalCredentials: getLocalCredentials,
     configure: core.configure
 };
